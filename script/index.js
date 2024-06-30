87bad0969f00e66433995e93196ae887c1172802
Tomás Manoukian <tomanoukian@gmail.com>
2024-06-30 19:50:08 -0300

    modificacion del fetch

--- script/index.js
+++ script/index.js
@@ -110,7 +110,11 @@ function generaSeries() {
 const baseURL = 'https://tpback-kl98.onrender.com';
 
 const getDatos = (endpoint)=> {
-    return fetch(`${baseURL}${endpoint}`)
+    return fetch(`${baseURL}${endpoint}`, {
+        method: 'GET',
+        credentials: 'include'
+    }
+    )
         .then(response => response.json())
         .catch(error => {
             console.error('Error al ingresar al endpoint', error);
