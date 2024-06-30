125180926ed27da4ea1ae097c759f0835f3e67f1
Tomás Manoukian <tomanoukian@gmail.com>
2024-06-30 19:16:33 -0300

    front

---
+++ script/getDatos.js
@@ -0,0 +1,10 @@
+// URL base da API
+const baseURL = 'https://tpback-kl98.onrender.com';
+
+export default function getDatos(endpoint) {
+    return fetch(`${baseURL}${endpoint}`)
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Error al ingresar al endpoint', error);
+        });
+}
